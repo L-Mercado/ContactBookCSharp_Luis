25d37e1bd88354a58b40a9e9a8f73d39bcf4ac6c
Luis Mercado <frozenreality14@gmail.com>
2026-05-05 03:31:27 -0400

    Add: ShowWelcomeScreen

---
+++ src/ContactBook/ContactBook.cs
@@ -0,0 +1,121 @@
+
+namespace ContactBook;
+
+public class ContactBook
+{
+    public const string NEXT_PAGE = "+";
+    public const string PREV_PAGE = "-";
+    public const string GOTO_PAGE = "G";
+    public const string PAGE_SIZE = "S";
+    public const string CREATE_CONTACT = "C";
+    public const string REVIEW_CONTACT = "R";
+    public const string UPDATE_CONTACT = "U";
+    public const string DELETE_CONTACT = "D";
+    public const string FIND_CONTACT = "F";
+    public const string ORDER_CONTACT = "O";
+    public const string DUPLICATE_CONTACT = "M";
+    public const string EXIT = "X";
+
+    public readonly string[] COMMANDS = new string[]
+    {
+        NEXT_PAGE,
+        PREV_PAGE,
+        GOTO_PAGE,
+        PAGE_SIZE,
+        CREATE_CONTACT,
+        REVIEW_CONTACT,
+        UPDATE_CONTACT,
+        DELETE_CONTACT,
+        FIND_CONTACT,
+        ORDER_CONTACT,
+        DUPLICATE_CONTACT,
+        EXIT
+    };
+
+
+    public ContactBook()
+    {
+        
+    }
+
+    public void Start()
+    {
+        ShowWelcomeScreen();
+
+        string input;
+        do
+        {
+            ShowContacts();
+
+            do
+            {
+                ShowInputOptions();
+                input = GetInput();
+            }
+            while (!IsValidInput(input));
+           
+            ProcessInput(input);
+        }
+        while(!ConfirmExit());
+
+        ShowExitScreen();
+    }
+
+    private void ShowWelcomeScreen()
+    {
+        Console.WriteLine("Welcome To My Contact Book");
+        PressEnterContinue();
+    }
+
+    private void ShowContacts()
+    {
+        
+    }
+
+    private void ShowInputOptions()
+    {
+        
+    }
+
+    private string GetInput()
+    {
+        return "";
+    }
+
+    private bool IsValidInput(string input)
+    {
+        return true;
+    }
+
+    private void ProcessInput(string input)
+    {
+        
+    }
+
+    private bool ConfirmExit()
+    {
+        return true;
+    }
+
+    private void ShowExitScreen()
+    {
+        
+    }
+
+  private void PressEnterContinue()
+{
+    Console.WriteLine("\nPress any key to continue...");
+    
+    
+    if (Console.IsInputRedirected)
+    {
+        
+        Console.ReadLine();
+    }
+    else
+    {
+        
+        Console.ReadKey();
+    }
+}
+}
--- src/ContactBook/Program.cs
+++ src/ContactBook/Program.cs
@@ -3,11 +3,8 @@ public class Program
 {
     public static void Main()
     {
-        
-       Contact c1 = new Contact();
-       Contact c2 = new Contact("Luis");
-       Contact c3 = new Contact("Prinima", "Mercado");
-       Contact c4 = new Contact("Gerardo", "Rosas", "123-786-1234");
-       Contact c5 = new Contact("Hector", "Rosas", "123-732-9872", "hector@gmail.com");
+        var cb = new ContactBook();
+        cb.Start();
+      
     }
 }
