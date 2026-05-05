80a6b76be708dd003f36f7d55ca504935305e613
Luis Mercado <frozenreality14@gmail.com>
2026-05-05 04:59:44 -0400

    Add: ShowContacts

---
+++ src/ContactBook/ContactSeed.cs
@@ -0,0 +1,106 @@
+namespace ContactBook;
+
+public static class ContactSeed
+{
+    public static List<Contact> Contacts = new List<Contact>()
+    {
+                new Contact ("John", "Smith", "212-555-1234", "john.smith@gmail.com"),
+                new Contact ("Sarah", "Johnson", "310-555-2345", "sarah.j@yahoo.com"),
+                new Contact ("Michael", "Williams", "415-555-3456", "michael.williams@hotmail.com"),
+                new Contact ("Emily", "Brown", "617-555-4567", "emily.brown@outlook.com"),
+                new Contact ("David", "Jones", "702-555-5678", "djones@gmail.com"),
+                new Contact ("Jessica", "Garcia", "305-555-6789", "jessica.garcia@aol.com"),
+                new Contact ("Christopher", "Miller", "206-555-7890", "chris.miller@protonmail.com"),
+                new Contact ("Amanda", "Davis", "512-555-8901", "amanda.davis@icloud.com"),
+                new Contact ("Daniel", "Rodriguez", "303-555-9012", "daniel.rodriguez@mail.com"),
+                new Contact ("Ashley", "Martinez", "404-555-0123", "ashley.m@yahoo.com"),
+                new Contact ("James", "Hernandez", "713-555-1234", "james.h@gmail.com"),
+                new Contact ("Jennifer", "Lopez", "602-555-2345", "jennifer.lopez@hotmail.com"),
+                new Contact ("Robert", "Gonzalez", "215-555-3456", ""), 
+                new Contact ("Linda", "Wilson", "312-555-4567", "linda.wilson@outlook.com"),
+                new Contact ("William", "Anderson", "313-555-5678", "william.a@gmail.com"),
+                new Contact ("Elizabeth", "Thomas", "414-555-6789", "ethomas@yahoo.com"),
+                new Contact ("Richard", "Taylor", "425-555-7890",  ""), 
+                new Contact ("Susan", "Moore", "510-555-8901", "susan.moore@icloud.com"),
+                new Contact ("Joseph", "Jackson", "514-555-9012", "joseph.jackson@gmail.com"), // Missing phone
+                new Contact ("Karen", "Martin", "518-555-0123", "karen.martin@aol.com"),
+                new Contact ("Thomas", "Lee", "519-555-1234", "thomas.lee@protonmail.com"),
+                new Contact ("Nancy", "Perez", "530-555-2345", "nancy.perez@gmail.com"),
+                new Contact ("Charles", "Thompson", "559-555-3456",  ""), // Missing both phone and email
+                new Contact ("Lisa", "White", "562-555-4567", "lisa.white@hotmail.com"),
+                new Contact ("Matthew", "Harris", "585-555-5678", "matthew.h@outlook.com"),
+                new Contact ("Margaret", "Sanchez", "604-555-6789", "margaret.sanchez@gmail.com"),
+                new Contact ("Anthony", "Clark", "605-555-7890", "anthony.clark@yahoo.com"),
+                new Contact ("Sandra", "Ramirez", "607-555-8901", "sandra.ramirez@mail.com"),
+                new Contact ("Mark", "Lewis", "608-555-9012", "mark.lewis@gmail.com"), // Missing phone
+                new Contact ("Betty", "Robinson", "609-555-0123", "betty.r@aol.com"),
+                new Contact ("Paul", "Walker", "610-555-1234", ""), // Missing email
+                new Contact ("Donna", "Young", "612-555-2345", "donna.young@hotmail.com"),
+                new Contact ("Steven", "Allen", "613-555-3456", "steven.allen@icloud.com"),
+                new Contact ("Kimberly", "King", "614-555-4567", "kimberly.king@gmail.com"),
+                new Contact ("Andrew", "Wright", "615-555-5678", "andrew.wright@protonmail.com"),
+                new Contact ("Michelle", "Scott", "616-555-6789", "michelle.scott@outlook.com"),
+                new Contact ("Kenneth", "Torres", "617-555-7890", "kenneth.t@gmail.com"), // Missing phone
+                new Contact ("Dorothy", "Nguyen", "618-555-8901", "dorothy.nguyen@yahoo.com"),
+                new Contact ("Joshua", "Hill", "619-555-9012", ""), // Missing contact info
+                new Contact ("Carol", "Flores", "620-555-0123", "carol.flores@gmail.com"),
+                new Contact ("Kevin", "Green", "623-555-1234", "kevin.green@aol.com"),
+                new Contact ("Amanda", "Adams", "626-555-2345", "amanda.adams@mail.com"),
+                new Contact ("Brian", "Nelson", "630-555-3456", "brian.nelson@hotmail.com"),
+                new Contact ("Melissa", "Baker", "631-555-4567", "melissa.baker@gmail.com"),
+                new Contact ("George", "Hall", "636-555-5678", "george.hall@yahoo.com"), // Missing phone
+                new Contact ("Deborah", "Rivera", "641-555-6789", "deborah.rivera@outlook.com"),
+                new Contact ("Edward", "Campbell", "646-555-7890", "edward.campbell@icloud.com"),
+                new Contact ("Stephanie", "Mitchell", "212-555-8901", "stephanie.mitchell@gmail.com"),
+                new Contact ("Jason", "Carter", "310-555-9012", "jason.carter@aol.com"), // Missing phone
+                new Contact ("Rebecca", "Roberts", "415-555-0123", "rebecca.roberts@protonmail.com"),
+                new Contact ("Frank", "Phillips", "617-555-1234", "frank.phillips@yahoo.com"),
+                new Contact ("Laura", "Campbell", "702-555-2345", "laura.campbell@gmail.com"),
+                new Contact ("Ryan", "Parker", "305-555-3456", ""), // Missing email
+                new Contact ("Megan", "Evans", "206-555-4567", "megan.evans@hotmail.com"),
+                new Contact ("Eric", "Edwards", "512-555-5678", "eric.edwards@outlook.com"),
+                new Contact ("Rachel", "Collins", "303-555-6789", "rachel.collins@aol.com"),
+                new Contact ("Justin", "Stewart", "404-555-7890", "justin.stewart@gmail.com"),
+                new Contact ("Heather", "Morris", "713-555-8901", "heather.morris@yahoo.com"),
+                new Contact ("Christian", "Rogers", "602-555-9012", "christian.r@icloud.com"), // Missing phone
+                new Contact ("Danielle", "Reed", "215-555-0123", "danielle.reed@gmail.com"),
+                new Contact ("Brandon", "Cook", "312-555-1234", "brandon.cook@hotmail.com"),
+                new Contact ("Samantha", "Morgan", "313-555-2345", "samantha.morgan@outlook.com"),
+                new Contact ("Austin", "Bell", "414-555-3456", "austin.bell@gmail.com"), // Missing phone
+                new Contact ("Brittany", "Murphy", "425-555-4567", "brittany.murphy@yahoo.com"),
+                new Contact ("Zachary", "Bailey", "510-555-5678", "zachary.bailey@aol.com"),
+                new Contact ("Lauren", "Rivera", "514-555-6789", "lauren.rivera@gmail.com"),
+                new Contact ("Nathan", "Cooper", "518-555-7890", "nathan.cooper@protonmail.com"),
+                new Contact ("Olivia", "Richardson", "519-555-8901", "olivia.richardson@mail.com"),
+                new Contact ("Tyler", "Cox", "530-555-9012", ""), // Missing contact info
+                new Contact ("Alyssa", "Howard", "559-555-0123", "alyssa.howard@gmail.com"),
+                new Contact ("Jacob", "Ward", "562-555-1234", "jacob.ward@hotmail.com"),
+                new Contact ("Hannah", "Torres", "585-555-2345", "hannah.torres@outlook.com"),
+                new Contact ("Nicholas", "Peterson", "604-555-3456", "nicholas.peterson@yahoo.com"),
+                new Contact ("Victoria", "Gray", "605-555-4567", "victoria.gray@gmail.com"), // Missing phone
+                new Contact ("Dylan", "Ramirez", "607-555-5678", "dylan.ramirez@aol.com"),
+                new Contact ("Sydney", "James", "608-555-6789", "sydney.james@icloud.com"),
+                new Contact ("Ethan", "Watson", "609-555-7890", "ethan.watson@gmail.com"),
+                new Contact ("Morgan", "Brooks", "610-555-8901", ""), // Missing email
+                new Contact ("Logan", "Kelly", "612-555-9012", "logan.kelly@hotmail.com"),
+                new Contact ("Julia", "Sanders", "613-555-0123", "julia.sanders@outlook.com"),
+                new Contact ("Caleb", "Price", "614-555-1234", "caleb.price@gmail.com"),
+                new Contact ("Madison", "Bennett", "615-555-2345", "madison.bennett@yahoo.com"),
+                new Contact ("Connor", "Wood", "616-555-3456", "connor.wood@aol.com"), // Missing phone
+                new Contact ("Grace", "Barnes", "617-555-4567", "grace.barnes@gmail.com"),
+                new Contact ("Elijah", "Ross", "618-555-5678", "elijah.ross@protonmail.com"),
+                new Contact ("Abigail", "Henderson", "619-555-6789", "abigail.henderson@mail.com"),
+                new Contact ("Cameron", "Coleman", "620-555-7890", ""), // Missing contact info
+                new Contact ("Natalie", "Jenkins", "623-555-8901", "natalie.jenkins@gmail.com"),
+                new Contact ("Jack", "Perry", "626-555-9012", "jack.perry@hotmail.com"),
+                new Contact ("Emma", "Powell", "630-555-0123", "emma.powell@outlook.com"),
+                new Contact ("Luke", "Long", "631-555-1234", "luke.long@yahoo.com"),
+                new Contact ("Hailey", "Patterson", "636-555-2345", "hailey.p@aol.com"), // Missing phone
+                new Contact ("Gabriel", "Hughes", "641-555-3456", "gabriel.hughes@gmail.com"),
+                new Contact ("Zoe", "Flores", "646-555-4567", "zoe.flores@icloud.com"),
+                new Contact ("Isaac", "Washington", "212-555-5678", "isaac.washington@protonmail.com"),
+                new Contact ("Anna", "Butler", "310-555-6789", "anna.butler@gmail.com"),
+                new Contact ("Owen", "Simmons", "415-555-7890", "owen.simmons@yahoo.com"),
+                new Contact ("Mia", "Foster", "617-555-8901", "mia.foster@hotmail.com"), // Missing phone
+    };
+            };
